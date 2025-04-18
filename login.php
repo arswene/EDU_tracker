91af30fc418d3ec6dc6c44dee8c3b2e2643dfdda
arswene <hirwaarsene3@gmail.com>
2025-04-18 15:22:51 +0200

    login page creation

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend>login form</legend>
+            <label for="username">username</label><input type="text" name="username">
+            <br></br>
+            <label for="password">password</label><input type="password" name="password">
+            <br></br>
+            <button type="submit">login</button>
+        </fieldset>
+    </form>
+</body>
+</html>
